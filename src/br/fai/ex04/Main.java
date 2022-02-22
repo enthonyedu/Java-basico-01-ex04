2749b15b1c67b0a4ec702d8ac3130c3f1dbb2b59
Enthony <enthonyedu@gmail.com>
2022-02-22 03:18:27 -0300

    feat: Primeiro commit

---
+++ src/br/fai/ex04/Main.java
@@ -0,0 +1,22 @@
+package br.fai.ex04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Main app = new Main ();
+		app.start();
+
+	}
+	
+	private void start() {
+		int i= 0;
+		while(i <= 20) {
+			System.out.println("valor i: " + i);
+			i += 1;
+			
+		}
+	}
+
+}
